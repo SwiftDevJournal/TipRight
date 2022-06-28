c1945aa87a5f42aa8fcbeb347813b13438d553f1
Mark Szymczyk <mark@swiftdevjournal.com>
2022-06-27 20:34:27 -0400

    Initial Commit

---
+++ Shared/TipRightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipRightApp.swift
+//  Shared
+//
+//  Created by mark on 6/27/22.
+//
+
+import SwiftUI
+
+@main
+struct TipRightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

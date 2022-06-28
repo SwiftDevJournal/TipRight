d284458698c370cdb70224648ad0020dc5dda272
Mark Szymczyk <mark@swiftdevjournal.com>
2022-06-27 22:01:36 -0400

    Add properties to store the bill amount, the tip percentage, and the tip amount.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var billAmount: Double = 0
+    @State private var tipPercentage: Int = 20
+    @State private var tipAmount: Double = 0
+    
     var body: some View {
         Text("Hello, world!")
             .padding()

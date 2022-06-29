78f785dc6012997168954fb74fb80c99088185a3
Mark Szymczyk <mark@swiftdevjournal.com>
2022-06-29 15:20:50 -0400

    Copy the code from the Swift Playgrounds version for the iOS version's content view.

--- iOS/ContentView.swift
+++ iOS/ContentView.swift
@@ -8,8 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var billAmount: Double = 0
+    @State private var tipPercentage: Int = 20
+    @State private var tipAmount: Double = 0
+    
+    let currencyFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        return formatter
+    }()
+    let tipRange = 0...1000
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Text("Bill Amount:")
+                TextField("Bill Amount", value: $billAmount, formatter: currencyFormatter)
+                    .keyboardType(.numberPad)
+            }
+            HStack {
+                Text("Tip Percentage:")
+                TextField("Tip Percentage", value: $tipPercentage, formatter: NumberFormatter())
+                    .keyboardType(.decimalPad)
+                // Increment or decrement by 5 percent.
+                Stepper("", value: $tipPercentage, in: tipRange, step: 5)
+            }
+            Text("Tip Amount: \(tipAmount)")
+            Button("Calculate Tip") {
+                calculateTip()
+            }
+        }
+        .onChange(of: billAmount) { _ in
+            calculateTip()
+        }
+        .onChange(of: tipPercentage) { _ in
+            calculateTip()
+        }
+    }
+    
+    func calculateTip() {
+        let percentage = Double(tipPercentage) / 100.0
+        tipAmount = billAmount * percentage
     }
 }
 

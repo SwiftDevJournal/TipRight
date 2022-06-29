435f65542cbd896e63f8b83836efdf43bb429f1b
Mark Szymczyk <mark@swiftdevjournal.com>
2022-06-29 15:18:22 -0400

    Add a file for the iOS version's content view.

---
+++ iOS/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  TipRight (iOS)
+//
+//  Created by mark on 6/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
